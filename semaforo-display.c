c917e947c24989b96200b1ed9c505f2dda388ca4
Joamerson Islan <joamersonislan38@gmail.com>
2025-01-26 16:01:13 -0300

    refactor: finalizada a refatoração de código

--- semaforo-display.c
+++ semaforo-display.c
@@ -1,11 +1,8 @@
-#include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include "hardware/i2c.h"
 #include "inc/ssd1306.h"
-#include "pico/binary_info.h"
 #include "pico/stdlib.h"
 
 #define LED_R_PIN 13
@@ -49,6 +46,15 @@ char *TEXTO_SEMAFORO_PARE[] = { // Texto do OLED quando a luz vermelha está ace
 
 int A_state = 0; // Botão A vem definido como desligado por padrão
 
+struct render_area frame_area = {
+  start_column : 0,
+  end_column : ssd1306_width - 1,
+  start_page : 0,
+  end_page : ssd1306_n_pages - 1
+};
+
+uint8_t ssd[ssd1306_buffer_length];
+
 void MostrarTexto(char *text[], uint8_t *ssd, struct render_area *frame_area, size_t line_count) { // Mostra o texto na tela
   memset(ssd, 0, ssd1306_buffer_length);
   int y_axis = 0;
@@ -65,18 +71,21 @@ void SinalAberto() {
   gpio_put(LED_R_PIN, 0);
   gpio_put(LED_G_PIN, 1);
   gpio_put(LED_B_PIN, 0);
+  MostrarTexto(TEXTO_SEMAFORO_SIGA, ssd, &frame_area, count_of(TEXTO_SEMAFORO_SIGA));
 }
 
 void SinalAtencao() {
   gpio_put(LED_R_PIN, 1);
   gpio_put(LED_G_PIN, 1);
   gpio_put(LED_B_PIN, 0);
+  MostrarTexto(TEXTO_SEMAFORO_ATENCAO, ssd, &frame_area, count_of(TEXTO_SEMAFORO_ATENCAO));
 }
 
 void SinalFechado() {
   gpio_put(LED_R_PIN, 1);
   gpio_put(LED_G_PIN, 0);
   gpio_put(LED_B_PIN, 0);
+  MostrarTexto(TEXTO_SEMAFORO_PARE, ssd, &frame_area, count_of(TEXTO_SEMAFORO_PARE));
 }
 
 int EsperarPressionamentoBotao(int timeMS) { // Checa constantemente se BTN_A está pressionado
@@ -118,38 +127,25 @@ void InicializarPerifericos() {
 
 int main() {
   InicializarPerifericos();
-  struct render_area frame_area = {
-    start_column : 0,
-    end_column : ssd1306_width - 1,
-    start_page : 0,
-    end_page : ssd1306_n_pages - 1
-  };
 
   calculate_render_area_buffer_length(&frame_area);
-  uint8_t ssd[ssd1306_buffer_length];
 
   while (true) {
-    SinalFechado(); // Sinal fechado por 8 segundos
-    MostrarTexto(TEXTO_SEMAFORO_PARE, ssd, &frame_area, count_of(TEXTO_SEMAFORO_PARE));
-
-    A_state = EsperarPressionamentoBotao(8000); // Waits for the button to be pressed
+    SinalFechado();                             // Sinal fechado por 8 segundos
+    A_state = EsperarPressionamentoBotao(8000); // Aguarda o pressionamento do botão
 
     if (A_state) {    // Quando o botão é pressionado...
       SinalAtencao(); // Sinal de atenção por 5 segundos
-      MostrarTexto(TEXTO_SEMAFORO_ATENCAO, ssd, &frame_area, count_of(TEXTO_SEMAFORO_ATENCAO));
       sleep_ms(5000);
 
       SinalAberto(); // Sinal aberto por 10 segundos
-      MostrarTexto(TEXTO_SEMAFORO_SIGA, ssd, &frame_area, count_of(TEXTO_SEMAFORO_SIGA));
       sleep_ms(10000);
 
     } else {          // Quando o botão não é pressionado...
       SinalAtencao(); // Sinal de atenção por 2 segundos
-      MostrarTexto(TEXTO_SEMAFORO_ATENCAO, ssd, &frame_area, count_of(TEXTO_SEMAFORO_ATENCAO));
       sleep_ms(2000);
 
       SinalAberto(); // Sinal aberto por 8 segundos
-      MostrarTexto(TEXTO_SEMAFORO_SIGA, ssd, &frame_area, count_of(TEXTO_SEMAFORO_SIGA));
       sleep_ms(8000);
     }
   }
